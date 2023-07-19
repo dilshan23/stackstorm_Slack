f69176c29adac706820d9a7cd061ebdcb67787fc
dilshan <dilshan@bst.com>
2023-07-19 22:59:13 +0530

    test

--- sensors/slack_sensor.py
+++ sensors/slack_sensor.py
@@ -25,28 +25,28 @@ class SampleSensor(Sensor):
 
         self._client = slack.WebClient(token=self._config['token'])
         self.messages = self._client.conversations_history(channel="C01NY5BN06S")
-        # messages = self._client.conversations_history(channel="C01NY5BN06S")
+        messages = self._client.conversations_history(channel="C01NY5BN06S")
 
-        # processed_emails = set()
+        processed_emails = set()
 
-        # import re
-        # for mes in messages["messages"]:
-        #     text = mes["text"]
+        import re
+        for mes in messages["messages"]:
+            text = mes["text"]
 
-        #     # Define the regex pattern
-        #     pattern = r'bot email to (\S+@[^.]+\.[a-zA-Z]+)'
+            # Define the regex pattern
+            pattern = r'bot email to (\S+@[^.]+\.[a-zA-Z]+)'
 
-        #     # Use re.search to find the match
-        #     match = re.search(pattern, text)
+            # Use re.search to find the match
+            match = re.search(pattern, text)
 
-        #     if match:
-        #         payload1 = {"text":"test"}
-        #         email_address = match.group(1)           
-        #         text1 = "sending email to "+email_address
-        #         if text1 not in processed_emails:
-        #             processed_emails.add(text1)
-        #             self.sensor_service.dispatch(trigger="slack_dilshan.new_update", payload=payload1,trace_tag="1234")
-        #             self._client.chat_postMessage(text=text1, channel="C01NY5BN06S")
+            if match:
+                payload1 = {"text":"test"}
+                email_address = match.group(1)           
+                text1 = "sending email to "+email_address
+                if text1 not in processed_emails:
+                    processed_emails.add(text1)
+                    self.sensor_service.dispatch(trigger="slack_dilshan.new_update", payload=payload1,trace_tag="1234")
+                    self._client.chat_postMessage(text=text1, channel="C01NY5BN06S")
                     
     def run(self):
         """
