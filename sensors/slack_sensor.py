fe03e0bb903f857d82090bc3be00a1dc285b3e57
dilshan <dilshan@bst.com>
2023-07-19 21:46:36 +0530

    test

--- sensors/slack_sensor.py
+++ sensors/slack_sensor.py
@@ -144,7 +144,7 @@ class SampleSensor(Sensor):
 
         while not self._stop:
             #self._logger.debug("HelloSensor dispatching trigger...")
-            count = self.sensor_service.get_value("dilshan_slack.count") or 0
+            count = self.sensor_service.get_value("slack_dilshan.count") or 0
             count = count + 1
 
             #slack
@@ -152,9 +152,11 @@ class SampleSensor(Sensor):
             for mes in messages["messages"]:
                 print(mes["text"])
                 if "email" in mes["text"]:
+                    payload = {}
+                    payload["text"] = "test"
                     #x = requests.post(url, json = payload)
-                    self.sensor_service.dispatch(trigger="dilshan_slack.new_update", payload=payload,trace_tag="1234")
-                    self.sensor_service.set_value("dilshan_slack.count", count)
+                    self.sensor_service.dispatch(trigger="slack_dilshan.new_update", payload=payload,trace_tag="1234")
+                    self.sensor_service.set_value("slack_dilshan.count", count)
             eventlet.sleep(60)
            
 
