46bd55331d4eebb50539b62d4197e3472238bc43
dilshan <dilshan@bst.com>
2023-07-20 20:38:33 +0530

    workflow init

--- start.sh
+++ start.sh
@@ -2,14 +2,16 @@ cd st2-docker
 sudo docker-compose up -d
 sudo docker-compose exec st2client bash
 
+#stop st2
+sudo docker-compose stop
 
-docker-compose stop
-
-
-###
+###edit and move
 
 nano packs/slack_dilshan/slack_dilshan.yaml
 cp packs/slack_dilshan/slack_dilshan.yaml /opt/stackstorm/configs/slack_dilshan.yaml
+
+
+## reload
 st2ctl reload --register-configs
 
 st2ctl reload --register-rules
