09047eab8b34914e29dc63b1768dc7be91a7280e
dilshan <dilshan@bst.com>
2023-07-20 09:16:43 +0530

    test

--- sensors/slack_sensor.py
+++ sensors/slack_sensor.py
@@ -27,30 +27,30 @@ class SampleSensor(Sensor):
 
         self._client = slack.WebClient(token=self._config['token'])
         self.messages = self._client.conversations_history(channel="C01NY5BN06S")
+        
+        processed_messages = []
         messages = self._client.conversations_history(channel="C01NY5BN06S")
-
-        processed_emails = set()
-
-        import re
         for mes in messages["messages"]:
-            text = mes["text"]
+            if mes["text"] not in processed_messages:
+                text = mes["text"]
 
-            #self._client.chat_postMessage(text=text, channel="C01NY5BN06S")
+                #self._client.chat_postMessage(text=text, channel="C01NY5BN06S")
 
-            # Define the regex pattern
-            pattern = r'bot email to (\S+@[^.]+\.[a-zA-Z]+)'
+                # Define the regex pattern
+                pattern = r'mailto(\S+@[^.]+\.[a-zA-Z]+)'
 
-            # Use re.search to find the match
-            match = re.search(pattern, text)
+                # Use re.search to find the match
+                match = re.search(pattern, text)
 
-            if match:
-                payload1 = {"text":"test"}
-                email_address = match.group(1)           
-                text1 = "sending email to "+email_address
-                if text1 not in processed_emails:
-                    processed_emails.add(text1)
+                if match:
+                    #payload1 = {"text":"test"}
+                    email_address = match.group(1)           
+                    text1 = "sending email to "+email_address
+                    
+                    
                     #self.sensor_service.dispatch(trigger="slack_dilshan.new_update", payload=payload1,trace_tag="1234")
                     self._client.chat_postMessage(text=text1, channel="C01NY5BN06S")
+                    processed_messages.append(mes["text"])
                     
     def run(self):
         """
