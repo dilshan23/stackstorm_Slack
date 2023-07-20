5b9bb64ae1c4ec700a47fffb063936864a52a3e8
dilshan <dilshan@bst.com>
2023-07-20 14:18:20 +0530

    test action

--- actions/send_message_to_slack.py
+++ actions/send_message_to_slack.py
@@ -3,6 +3,8 @@ from st2common.runners.base_action import Action
 
 class SendMessageToSlackAction(Action):
     def run(self,text="test"):
+        payload = self.config.get('new_update', {})
+        message = payload["text"]
         client = slack.WebClient(token=self.config['token'])
         m = client.chat_postMessage(text="testing", channel=self.config['channel_id'])
         return m
--- sensors/slack_sensor.py
+++ sensors/slack_sensor.py
@@ -40,8 +40,10 @@ class SlackSensor(Sensor):
                     match = re.search(pattern, text)
                     if match:
                         email_address = match.group(1)           
-                        text1 = "sending email to "+email_address         
-                        #self.sensor_service.dispatch(trigger="slack_dilshan.new_update", payload=payload1,trace_tag="1234")
+                        text1 = "sending email to "+email_address
+                        payload = {}
+                        payload["text"] = email_address         
+                        self.sensor_service.dispatch(trigger="slack_dilshan.new_update", payload=payload,trace_tag="1234")
                         self._client.chat_postMessage(text=text1, channel="C01NY5BN06S")
                         processed_messages.append(mes["text"])
 
