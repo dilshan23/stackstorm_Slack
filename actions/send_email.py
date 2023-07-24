e2df5cf470702d9daa6a62b92d68303460302e5b
dilshan <dilshan@bst.com>
2023-07-24 07:38:13 +0530

    worflow test

--- actions/send_email.py
+++ actions/send_email.py
@@ -1,20 +1,46 @@
 import smtplib
 from st2common.runners.base_action import Action
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 class SendMessageToSlackAction(Action):
 	# creates SMTP session
-	def run(self,receiver_email="test",message="test message"):
-		s = smtplib.SMTP('smtp.gmail.com', 587)
+	def run(self,receiver_email):
 
-		# start TLS for security
-		s.starttls()
+		sender_email=self.config['sender_email']
+		sender_password = self.config['sender_password']
+		subject = self.config['subject']
+		body = self.config['body']
 
-		# Authentication
-		s.login("sender_email_id", "sender_email_id_password")
+		
+	    # Set up the MIMEText object for the email content
+	    msg = MIMEMultipart()
+	    msg['From'] = sender_email
+	    msg['To'] = receiver_email
+	    msg['Subject'] = subject
+
+	    # Attach the email body
+	    msg.attach(MIMEText(body, 'plain'))
+
+	    # Connect to the SMTP server
+	    try:
+	        with smtplib.SMTP('mail.privateemail.com', 587 ) as server:  # Replace with your SMTP server and port
+	            server.starttls()
+	            # Log in to the SMTP server
+	            server.login(sender_email, sender_password)
+
+	            # Send the email
+	            server.sendmail(sender_email, receiver_email, msg.as_string())
+	            print("Email sent successfully!")
+
+	    except Exception as e:
+	        print("Error sending email:", str(e))
 
 
-		# sending the mail
-		s.sendmail("sender_email_id", receiver_email, message)
+		
 
-		# terminating the session
-		s.quit()
+
+
+
