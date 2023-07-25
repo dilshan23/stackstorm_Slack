146381f324df5c2af721eb7862375e05a8e2cba0
dilshan <dilshan@bst.com>
2023-07-25 11:00:09 +0530

    test

--- actions/send_message_to_slack.py
+++ actions/send_message_to_slack.py
@@ -2,11 +2,12 @@ import slack
 from st2common.runners.base_action import Action
 
 class SendMessageToSlackAction(Action):
-    def run(self,text="test"):
+    def run(self,text="test",whatsapp_status="ok"):
         if text is not None:
             text = "sent email to " + text
         else:
             text = "email is None"  # Or some default message if text is None
+        text = text+whatsapp_status
         client = slack.WebClient(token=self.config['token'])
         m = client.chat_postMessage(text=text, channel=self.config['channel_id'])
         return m
