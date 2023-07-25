0e56a294239dd34ec147f478091a46f1f781e369
dilshan <dilshan@bst.com>
2023-07-25 09:46:04 +0530

    test

--- actions/send_sms.py
+++ actions/send_sms.py
@@ -5,26 +5,33 @@ import requests
 class SendSms(Action):
 	# creates SMTP session
 	def run(self,receiver_sms):
+
+		if receiver_sms != "NA":
 		
 
-		# Sample JSON data for the request body
-		sample_body = {
-		    "sms": receiver_sms
-		    
-		}
+			# Sample JSON data for the request body
+			sample_body = {
+			    "sms": receiver_sms
+			    
+			}
 
-		# URL to send the POST request to
-		url = "https://3a6f-175-157-233-97.ngrok-free.app"
+			# URL to send the POST request to
+			url = "https://3a6f-175-157-233-97.ngrok-free.app"
 
-		# Send the POST request
-		response = requests.post(url, json=sample_body)
+			# Send the POST request
+			response = requests.post(url, json=sample_body)
 
-		# Check the response status code
-		if response.status_code == 200:
-		    print("Request sent ")
+			# Check the response status code
+			if response.status_code == 200:
+			    print("Request sent ")
+			    return "SMS ok"
+			else:
+			    print(f"Request failed with status code: {response.status_code}")
+			    print(response.text)  # Print response content for debugging if needed
+			    return "SMS not ok"
+
 		else:
-		    print(f"Request failed with status code: {response.status_code}")
-		    print(response.text)  # Print response content for debugging if needed
+			print("SMS NA")
 
 
 			
--- actions/send_whatsapp.py
+++ actions/send_whatsapp.py
@@ -5,24 +5,32 @@ class SendWhatsapp(Action):
 	# creates SMTP session
 	def run(self,receiver_whatsapp_number):
 
-		# Sample JSON data for the request body
-		sample_body = {
-		    "whatsapp": receiver_whatsapp_number
-		    
-		}
+		if receiver_whatsapp_number != "NA":
 
-		# URL to send the POST request to
-		url = "https://3a6f-175-157-233-97.ngrok-free.app"
+			# Sample JSON data for the request body
+			sample_body = {
+			    "whatsapp": receiver_whatsapp_number
+			    
+			}
 
-		# Send the POST request
-		response = requests.post(url, json=sample_body)
+			# URL to send the POST request to
+			url = "https://3a6f-175-157-233-97.ngrok-free.app"
 
-		# Check the response status code
-		if response.status_code == 200:
-		    print("Whatsapp message sent ")
+			# Send the POST request
+			response = requests.post(url, json=sample_body)
+
+			# Check the response status code
+			if response.status_code == 200:
+			    print("Whatsapp message sent ")
+			    return "Whatsapp ok"
+			else:
+			    print(f"Request failed with status code: {response.status_code}")
+			    print(response.text)  # Print response content for debugging if needed
+			    return "Whatsapp not ok"
+
+
 		else:
-		    print(f"Request failed with status code: {response.status_code}")
-		    print(response.text)  # Print response content for debugging if needed
+			print("NA for Whatsapp")
 
 		
 	            
